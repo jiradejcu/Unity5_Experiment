94febb09f4b646f7c3e0505afe27d07670c75c08
Jiradej Vatanutanon <jiradej_cu@hotmail.com>
2015-10-11 21:59:42 +0700

    add auto car driving script

---
+++ UI Test/Assets/Script/CarDrive.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityStandardAssets.Vehicles.Car;
+
+public class CarDrive : MonoBehaviour
+{
+	public CarController carController;
+	float? curve;
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.R)) {
+			curve = 1f;
+			gameObject.transform.position = Vector3.zero;
+			gameObject.transform.rotation = Quaternion.identity;
+		}
+		
+		if (curve.HasValue) {
+			float accel = 1f;				
+			carController.Move (curve.Value, accel, accel, 0f);
+
+			if(gameObject.transform.rotation.y > 90)
+				curve = null;
+		}
+	}
+}
