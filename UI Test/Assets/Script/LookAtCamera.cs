8257dad9284bae513ccfc3a47d18f6dd97dc4316
Jiradej Vatanutanon <jiradej_cu@hotmail.com>
2015-11-06 16:21:43 +0700

    "LookAtCamera" script

---
+++ UI Test/Assets/Script/LookAtCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtCamera : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 direction = Camera.main.transform.forward;
+		Debug.Log (direction);
+		transform.rotation = Quaternion.LookRotation (-direction);
+	}
+}
