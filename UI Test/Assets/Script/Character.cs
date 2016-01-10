7c8051a4b87ae50fab82823c9e34c5f8381ac478
Jiradej Vatanutanon <jiradej_cu@hotmail.com>
2016-01-10 21:52:44 +0700

    create virtual button demo

---
+++ UI Test/Assets/Script/Character.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour
+{
+	public int characterNo;
+	MeshRenderer mr;
+
+	void Start ()
+	{
+		mr = GetComponentInChildren<MeshRenderer> ();
+	}
+
+	public void SetVisible (bool visible)
+	{
+		mr.enabled = visible;
+	}
+}
---
+++ UI Test/Assets/Script/VirtualButtonEventHandler.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Vuforia;
+
+public class VirtualButtonEventHandler : MonoBehaviour, IVirtualButtonEventHandler
+{
+	Character[] characterList;
+	int activeIndex = 0;
+
+	void Start ()
+	{
+		characterList = GetComponentsInChildren<Character> ();
+
+		// Register with the virtual buttons TrackableBehaviour
+		VirtualButtonBehaviour vb = GetComponentInChildren<VirtualButtonBehaviour> ();
+		if (vb) {
+			vb.RegisterEventHandler (this);
+		}
+	}
+
+	public void OnButtonPressed (VirtualButtonAbstractBehaviour vb)
+	{
+		Debug.Log ("OnButtonPressed::" + vb.VirtualButtonName);
+
+		if (!IsValid ()) {
+			return;
+		}
+
+		int i = 0;
+		foreach (Character c in characterList) {
+			c.SetVisible (i++ == activeIndex);
+		}
+
+		activeIndex = ++activeIndex % characterList.Length;
+	}
+
+	public void OnButtonReleased (VirtualButtonAbstractBehaviour vb)
+	{
+		Debug.Log ("OnButtonReleased::" + vb.VirtualButtonName);
+
+		if (!IsValid ()) {
+			return;
+		}
+	}
+	
+	private bool IsValid ()
+	{
+		return true;
+	}
+}
