14a150ef7e13d6a679d2c5e4a983ee5e30fb8296
Jiradej Vatanutanon <jiradej_cu@hotmail.com>
2016-04-06 10:36:03 +0700

    update project version and add linux test

---
+++ Linux Test/Assets/Scripts/InitBase.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitBase : MonoBehaviour
+{
+		// Use this for initialization
+		void Start ()
+		{
+				Debug.Log ("Linux Bot Started");
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+}
